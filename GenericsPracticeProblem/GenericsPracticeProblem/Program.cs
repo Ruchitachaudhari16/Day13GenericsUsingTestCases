8da38807f8763d0065e9aa2cdb9493c7292811e2
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-07-14 15:22:02 -0400

    [Ruchita]Add:3 Integrs numbers to find  Largest one

---
+++ GenericsPracticeProblem/GenericsPracticeProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPracticeProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Maximum maximum = new Maximum();
+            
+            Console.WriteLine("Enter the FirstNumber");
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the SecondNumber");
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the ThirdNumber");
+            int thirdValue = Convert.ToInt32(Console.ReadLine());
+            int val = maximum.Number(firstValue, secondValue, thirdValue);
+            Console.WriteLine("{0} is largest number ", val);
+            Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+    }
+
